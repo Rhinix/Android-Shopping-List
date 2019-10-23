e11c2a937360974d02118d88589095429ae8ea10
Rhinix <amella-alessio}live.be>
2019-10-23 18:32:37 +0200

    adding bcrypt package

--- API/app.js
+++ API/app.js
@@ -4,16 +4,45 @@ const morgan = require("morgan");
 const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
 
+const userRoutes = require("./api/routes/user");
+
+const connectionString =
+  "mongodb+srv://" +
+  process.env.MONGO_ATLAS_USER +
+  ":" +
+  process.env.MONGO_ATLAS_PSWD +
+  "@node-rest-shop-rqiqg.mongodb.net/ShoppingList?retryWrites=true&w=majority";
+
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .catch(err => {
+    console.log(err);
+  });
+mongoose.Promise = global.Promise;
+
 app.use(morgan("dev"));
 
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
-//mongoose.connect();
-//mongoose.Promise=global.Promise;
+app.use("/user", userRoutes);
 
-app.use("/", (req, res) => {
-  res.status(200).json({ message: "ça fonctionne" });
+//handling error
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res) => {
+  res.status(error.status || 500).json({
+    error: {
+      message: error.message
+    }
+  });
 });
 
 module.exports = app;
