5460b29b8fc82beb7c1fe8ac8864214b7c0f25e4
Alessio Amella <amella-alessio@live.be>
2019-11-27 14:37:16 +0100

    adding listFragment class

---
+++ ShoppingList/app/src/main/java/be/amellaa/shoppinglist/activities/shoppingListActivity/ListFragment.kt
@@ -0,0 +1,53 @@
+package be.amellaa.shoppinglist.activities.shoppingListActivity
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import be.amellaa.shoppinglist.R
+import be.amellaa.shoppinglist.dao.CommunicationInterface
+import be.amellaa.shoppinglist.dao.ShoppingListDAO
+import be.amellaa.shoppinglist.models.ShoppingList
+
+abstract class ListFragment() : Fragment() {
+
+    lateinit var mRecyclerView: RecyclerView
+    lateinit var swipeView: SwipeRefreshLayout
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view: View = inflater.inflate(R.layout.shoppinglist_fragment, container, false)
+        mRecyclerView = view.findViewById(R.id.recyclerView)
+        mRecyclerView.layoutManager = LinearLayoutManager(activity)
+        swipeView = view.findViewById<SwipeRefreshLayout>(R.id.swipeRefresh) as SwipeRefreshLayout
+        swipeView.setOnRefreshListener { getList() }
+        getList()
+        return view
+    }
+
+    abstract fun getList()
+
+    protected fun setShoppingList(newShoppingList: ArrayList<ShoppingList>) {
+        val adapterShopping: ShoppingListAdapter =
+            ShoppingListAdapter(
+                newShoppingList
+            )
+        mRecyclerView.adapter = adapterShopping
+        (mRecyclerView.adapter as ShoppingListAdapter).notifyDataSetChanged()
+        stopRefreshing()
+    }
+
+    private fun stopRefreshing() {
+        if (swipeView.isRefreshing) {
+            swipeView.isRefreshing = false
+        }
+    }
+
+}
--- ShoppingList/app/src/main/java/be/amellaa/shoppinglist/activities/shoppingListActivity/MyListFragment.kt
+++ ShoppingList/app/src/main/java/be/amellaa/shoppinglist/activities/shoppingListActivity/MyListFragment.kt
@@ -13,45 +13,14 @@ import be.amellaa.shoppinglist.dao.CommunicationInterface
 import be.amellaa.shoppinglist.dao.ShoppingListDAO
 import be.amellaa.shoppinglist.models.ShoppingList
 
-class MyListFragment() : Fragment() {
-
-    lateinit var mRecyclerView: RecyclerView
-    lateinit var swipeView: SwipeRefreshLayout
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var view : View = inflater.inflate(R.layout.shoppinglist_fragment, container, false)
-        mRecyclerView = view.findViewById(R.id.recyclerView)
-        mRecyclerView.layoutManager = LinearLayoutManager(activity)
-        swipeView = view.findViewById<SwipeRefreshLayout>(R.id.swipeRefresh) as SwipeRefreshLayout
-        swipeView.setOnRefreshListener { getMyList() }
-        getMyList()
-        return view
-    }
-
-    private fun getMyList() {
-        ShoppingListDAO.instance.getMyList(object: CommunicationInterface {
+class MyListFragment : ListFragment() {
+    override fun getList() {
+        ShoppingListDAO.instance.getMyList(object : CommunicationInterface {
             override fun communicateShoppingLists(shoppingLists: ArrayList<ShoppingList>) {
-                activity?.runOnUiThread{
+                activity?.runOnUiThread {
                     setShoppingList(shoppingLists)
                 }
             }
         })
     }
-
-    private fun setShoppingList(newShoppingList: ArrayList<ShoppingList>) {
-        val adapterShopping: ShoppingListAdapter =
-            ShoppingListAdapter(
-                newShoppingList
-            )
-        mRecyclerView.adapter = adapterShopping
-        (mRecyclerView.adapter as ShoppingListAdapter).notifyDataSetChanged()
-        stopRefreshing()
-    }
-
-    private fun stopRefreshing() {
-        if (swipeView.isRefreshing) {
-            swipeView.isRefreshing = false
-        }
-    }
-
 }
